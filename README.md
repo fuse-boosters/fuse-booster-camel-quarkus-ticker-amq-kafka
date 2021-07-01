3eba25c4a7ca7c9869d40a934b67a39673071114
Thomas Diesler <tdiesler@redhat.com>
2021-07-01 12:26:39 +0200

    Make currency pair configurable

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
-# fuse-booster-quarkus-amq-kafka project
+# fuse-booster-camel-quarkus-ticker2log
 
 This project uses Quarkus, the Supersonic Subatomic Java Framework.
 
 If you want to learn more about Quarkus, please visit its website: [https://quarkus.io](https://quarkus.io).
 
+## Camel Components 
+
+* camel-quarkus-jackson
+* camel-quarkus-log
+* camel-quarkus-timer
+* camel-quarkus-xchange
 
 ## Packaging and running the application
 
@@ -15,14 +21,22 @@ The application can be packaged using:
 
 The application can now be run using `java -jar target/quarkus-app/quarkus-run.jar`.
 
+## Building Docker images
+
+The application can be packaged using:
+
+```shell script
+./mvnw clean package -Pdocker
+```
+
 ## Running with Docker
 
 ```shell script
 # Run all images
-docker-compose -f docs/compose/amq-kafka.yml up --detach \
-	&& docker logs -f kafka2log
+docker-compose -f docs/compose/ticker2log.yml up --detach \
+	&& docker logs -f ticker2log
 
 # Stop all images
-docker-compose -f docs/compose/amq-kafka.yml down
+docker-compose -f docs/compose/ticker2log.yml down
 ```
 
--- src/main/java/com/redhat/fuse/boosters/TickerRoute.java
+++ src/main/java/com/redhat/fuse/boosters/TickerRoute.java
@@ -3,20 +3,27 @@ package com.redhat.fuse.boosters;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import javax.enterprise.context.ApplicationScoped;
+
 import org.apache.camel.builder.RouteBuilder;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.knowm.xchange.dto.marketdata.Ticker;
 
+@ApplicationScoped
 public class TickerRoute extends RouteBuilder {
 
-    @Override
+	@ConfigProperty(name = "ticker2log.xchange.currencypair") 
+	String pair;
+	
+	@Override
     public void configure() throws Exception {
         from("timer:ticker?period=2500")
-	        .to("xchange:binance?service=marketdata&method=ticker&currencyPair=ada/usdt")
+	        .to("xchange:binance?service=marketdata&method=ticker&currencyPair=" + pair)
 	        .process(ex -> {
 	        	Ticker ticker = ex.getMessage().getBody(Ticker.class);
 	        	Map<String, Object> map = new LinkedHashMap<>();
 	        	map.put("pair", ticker.getInstrument());
-	        	map.put("close", ticker.getLast());
+	        	map.put("last", ticker.getLast());
 	        	ex.getMessage().setBody(map);
 	        })
 	        .marshal().json()
